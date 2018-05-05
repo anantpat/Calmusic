c71a9e58645a8cd4720c72099a1bfa0a89d74cfe
Anant Patni <anantpatni@anants-macbook-air-2.local>
2018-05-04 21:13:36 -0400

    Initial Commit

---
+++ Calmusic/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Calmusic
+//
+//  Created by Anant Patni on 04/05/18.
+//  Copyright © 2018 Anant & Angad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
